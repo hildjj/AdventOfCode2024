d7e51fdfe863b23f63af34b57978f8bd7c82a800
Joe Hildebrand <joe-github@cursive.net>
2024-12-18 12:09:13 -0700

    cleanup

--- day18.ts
+++ day18.ts
@@ -1,4 +1,4 @@
-import { AllDirs, Point, PointMap, Rect } from './lib/rect.ts';
+import { AllDirs, Point, PointMap, PointSet, Rect } from './lib/rect.ts';
 import { type MainArgs, parseFile } from './lib/utils.ts';
 import { BinaryHeap } from '@std/data-structures';
 
@@ -7,12 +7,13 @@ type Parsed = [x: number, y: number][];
 const SIZE = 71;
 const NUM = 1024;
 
-function astar(r: Rect): number {
+function astar(r: Rect): PointSet {
   const start = new Point(0, 0);
   const end = new Point(r.width - 1, r.height - 1);
 
   const gScore = new PointMap([[start, 0]]);
   const fScore = new PointMap([[start, 0]]);
+  const prev = new PointMap<Point>();
   const backlog = new BinaryHeap<Point>(
     (a, b) => fScore.get(a)! - fScore.get(b)!,
   );
@@ -23,7 +24,13 @@ function astar(r: Rect): number {
     const g = gScore.get(p)!;
 
     if (p.equals(end)) {
-      return g;
+      const ps = new PointSet([start]);
+      let pp: Point | undefined = p;
+      while (pp) {
+        ps.add(pp);
+        pp = prev.get(pp);
+      }
+      return ps;
     }
 
     for (const d of AllDirs) {
@@ -34,12 +41,13 @@ function astar(r: Rect): number {
           gScore.set(next, g + 1);
           fScore.set(next, g + next.manhattan(end));
           backlog.push(next);
+          prev.set(next, p);
         }
       }
     }
   }
 
-  return NaN;
+  return new PointSet();
 }
 
 function part1(inp: Parsed): number {
@@ -48,19 +56,27 @@ function part1(inp: Parsed): number {
     rect.set(...inp[i], '#');
   }
 
-  return astar(rect);
+  return astar(rect).size - 1; // Fencepost
 }
 
 function part2(inp: Parsed): [number, number] {
   const rect = Rect.ofSize(SIZE, SIZE, '.');
-  // 2.5 found empirically to just reduce run time.
-  for (let i = 0; i < 2.5 * NUM; i++) {
+  // We checked the first NUM in part 1
+  for (let i = 0; i < NUM; i++) {
     rect.set(...inp[i], '#');
   }
-  for (let i = 2.5 * NUM; i < inp.length; i++) {
-    rect.set(...inp[i], '#');
-    if (isNaN(astar(rect))) {
-      return inp[i];
+
+  // Only check if the point hits our existing path.
+  // Force the first one to match so we get one good path at the beginning.
+  let path = new PointSet([new Point(...inp[NUM])]);
+  for (let i = NUM; i < inp.length; i++) {
+    const p = new Point(...inp[i]);
+    rect.set(p, '#');
+    if (path.has(p)) {
+      path = astar(rect);
+      if (path.size === 0) {
+        return inp[i];
+      }
     }
   }
   return [NaN, NaN];
